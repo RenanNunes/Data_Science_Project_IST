55dfa878d71f84fb011f803902c75195fe8b5b66
Renan Nunes <renan_hamada@hotmail.com>
2019-10-18 18:46:09 +0100

    Decision tree model

--- modelAnalyzesFunctions.py
+++ modelAnalyzesFunctions.py
@@ -2,8 +2,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.tree import DecisionTreeClassifier
 import sklearn.metrics as metrics
 import itertools
+import graphFunctions as graph
 
 def naive_bayes_analyzes(X, y, labels, estimators, rskf, title_complement= '- '):
     accuracy = {}
@@ -33,4 +35,44 @@ def naive_bayes_analyzes(X, y, labels, estimators, rskf, title_complement= '- ')
         graph.plot_confusion_matrix(axs[0, 1], cnf_mtx[clf], labels, normalize=True, title_complement=(title_complement + clf))
         plt.tight_layout()
         plt.show()
+    return accuracy, sensitivity
+
+def decision_tree_analyzes(X, y, min_samples_leaf, max_depths, criteria, rskf):
+    accuracy = {}
+    sensitivity = {}
+
+    for c in criteria:
+        accuracy[c] = {}
+        sensitivity[c] = {}
+        for max_d in max_depths:
+            accuracy[c][max_d] = np.zeros((len(min_samples_leaf)))
+            sensitivity[c][max_d] = np.zeros((len(min_samples_leaf)))
+
+    for train_index, test_index in rskf.split(X, y):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        for c in range(len(criteria)):
+            crit = criteria[c]
+            for d in max_depths:
+                yvalues = []
+                recall = []
+                for n in min_samples_leaf:
+                    tree = DecisionTreeClassifier(min_samples_leaf=n, max_depth=d, criterion=crit)
+                    tree.fit(X_train, y_train)
+                    prdY = tree.predict(X_test)
+                    yvalues.append(metrics.accuracy_score(y_test, prdY))
+                    recall.append(metrics.recall_score(y_test, prdY))
+                accuracy[crit][d] += yvalues
+                sensitivity[crit][d] += recall
+
+    plt.figure()
+    fig, axs = plt.subplots(2, 2, figsize=(16, 12), squeeze=False)
+
+    for c in range(len(criteria)):
+        for max_d in max_depths:
+            accuracy[criteria[c]][max_d] /= rskf.get_n_splits()
+            sensitivity[criteria[c]][max_d] /= rskf.get_n_splits()
+        graph.multiple_line_chart(axs[0, c], min_samples_leaf, accuracy[criteria[c]], 'Decision Trees with %s criteria'%criteria[c], 'nr estimators', 'accuracy', percentage=True)
+        graph.multiple_line_chart(axs[1, c], min_samples_leaf, sensitivity[criteria[c]], 'Decision Trees with %s criteria'%criteria[c], 'nr estimators', 'sensitivity', percentage=True)
+    plt.show()
     return accuracy, sensitivity
